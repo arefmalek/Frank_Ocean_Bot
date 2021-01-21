6914f873998c1f762e647745d81f200a35d1f1d2
Aref Malek <arefmalek02@gmail.com>
2021-01-20 22:16:33 -0800

    randomized the posts now :)

--- redditFunctions.py
+++ redditFunctions.py
@@ -2,16 +2,22 @@ import praw
 import requests
 import os
 
+from random import randint
+
 
 def createImage(reddit, api):
+    choices = []
+
     for submission in reddit.subreddit("frankocean").search(
         'flair:"fan art"', 
-        limit=5,
+        limit=10,
         time_filter="day"
         ):
         if ".jpg" in submission.url:
-            post = submission
-            break
+            choices.append(submission)
+    
+    pick = randint(0, len(choices) - 1)
+    post = choices[pick]
     print(post.author)
     message = "Today's post is from u/" + str(post.author)
 
